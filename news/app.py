08190b0c4d1a4c38ba90d1ebf7aadd123e6dee3c
lisisharand <lisisharand@qq.com>
2018-10-12 15:47:46 +0800

    fix bug

--- news/app.py
+++ news/app.py
@@ -1,21 +1,21 @@
 from flask import Flask,render_template
 import os
-app = Flask()
+import json
+app = Flask(__name__)
 class Files(object):
-    directory  =os.path.join( os.path.abspath('__name__'),'..','files')
+    directory  =os.path.join( os.path.abspath(os.path.dirname(__name__)),'..','files')
+    def __init__(self):
+        self._files = self._read_all_files()
     def _read_all_files(self):
         result ={}
         filenames = os.listdir(self.directory)
         for filename in filenames:
             file_path = os.path.join(self.directory,filename)
-            with open(file) as f:
+            with open(file_path) as f:
                 result[filename[:-5]] = json.load(f)
-
-    return result
-    def __init__(self):
-    
-    def get_title_list():
-        return[item['title'] for item in self._fiels.values() ]
+        return result
+    def get_title_list(self):
+        return[item['title'] for item in self._files.values() ]
 
     def get_by_filename(self,filename):
         return self._files.get(filename)
@@ -29,12 +29,12 @@ def index():
 @app.route('/files/<filename>')
 def file(filename):
     file_item  = files.get_by_filename(filename)
-    if not file_item
+    if not file_item:
         abort(404)
     return render_template('file.html',file_item=file_item)
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'),404
-if __name__ == '__main__'
+if __name__ == '__main__':
     app.run()
 
