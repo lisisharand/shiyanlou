e5cd354514e97a37685011ac9dcac23a3db59246
lisisharand <lisisharand@qq.com>
2018-10-21 16:14:59 +0800

    challenge_query_data_from_MongoDB

---
+++ project_MongoDB/getrank.py
@@ -0,0 +1,30 @@
+import sys
+from pymongo import MongoClient
+import pandas as pd
+def get_rank(user_id):
+    #read data
+    client = MongoClient()
+    db = client.shiyanlou
+    contests = db.contests
+    data = pd.DataFrame(list(contests.find()))
+    #user_id is exist and get_rank
+    if user_id in list(data['user_id']):
+        group_data = data.groupby(['user_id'])['score','submit_time'].sum()
+        rank_data = group_data.sort_values(['submit_time']).sort_values(['score'],ascending=False)
+        reindex_data = rank_data.reset_index()#?
+        reindex_data['rank'] = reindex_data.index + 1
+        user_data = reindex_data[reindex_data['user_id']== user_id]
+        rank = int(user_data['rank'].values)
+        score = int(user_data['score'].values)
+        submit_time = int(user_data['submit_time'].values)
+    else:
+        print('NOTFIND')
+        exit()
+    return rank,score,submit_time
+if __name__ =='__main__':
+    if len(sys.argv) != 2:
+        print("parameter error")
+        exit(1)
+    user_id = sys.argv[1]
+    user_data = get_rank(int(user_id))
+    print(user_data)
