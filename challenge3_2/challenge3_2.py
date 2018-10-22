4bf95b0d1d8722990a151d9def8d43220626c4ea
lisisharand <lisisharand@qq.com>
2018-10-22 18:11:46 +0800

    fix bug

--- challenge3_2/challenge3_2.py
+++ challenge3_2/challenge3_2.py
@@ -1,27 +1,27 @@
 #-*- coding:utf-8 -*-
 import datetime
-from openpyxl import load_wordbook
+from openpyxl import load_workbook
 from openpyxl import Workbook
 
-wk = load_wordbook('courses.xlsx')
-student_sheet = wk['student']
+wk = load_workbook('courses.xlsx')
+student_sheet = wk['students']
 time_sheet = wk['time']
 
 def combine():
     combine_sheet = wk.create_sheet(title="combine")
-    combine_sheet.append('CreateTime','CourseName','StudentNum','StudyTime')
+    combine_sheet.append(['CreateTime','CourseName','StudentNum','StudyTime'])
     for stu in student_sheet.values:
         if stu[2]!= "学习人数":
             for time in time_sheet.values:
                 if time[1]== stu[1]:
                     combine_sheet.append(list(stu)+ [time[2]])
-    wk.save('course.xlsx')
+    wk.save('courses.xlsx')
 def split():
     combine_sheet = wk['combine']
     split_name = []
     for item  in combine_sheet.values:
-        if item[0] != "CreatTime":
-            split_name.append(tiem[0].strftime('%Y'))
+        if item[0] != "CreateTime":
+            split_name.append(item[0].strftime('%Y'))
    
     for name in set(split_name):
         wk_temp = Workbook()
