cfa3dcd294197cf6199895b91e570f3317460c0a
lisisharand <lisisharand@qq.com>
2018-10-22 22:31:53 +0800

    fix bug

--- challenge3_2/challenge3_2.py
+++ challenge3_2/challenge3_2.py
@@ -9,7 +9,7 @@ time_sheet = wk['time']
 
 def combine():
     combine_sheet = wk.create_sheet(title="combine")
-    combine_sheet.append(['CreateTime','CourseName','StudentNum','StudyTime'])
+    combine_sheet.append(['创建时间','课程名称','学习人数','学习时间'])
     for stu in student_sheet.values:
         if stu[2]!= "学习人数":
             for time in time_sheet.values:
@@ -20,15 +20,15 @@ def split():
     combine_sheet = wk['combine']
     split_name = []
     for item  in combine_sheet.values:
-        if item[0] != "CreateTime":
+        if item[0] != "创建时间":
             split_name.append(item[0].strftime('%Y'))
    
     for name in set(split_name):
         wk_temp = Workbook()
         wk_temp.remove(wk_temp.active)
-        ws = wk.create_sheet(title="name")
+        ws = wk_temp.create_sheet(title="name")
         for item_by_year in combine_sheet.values:
-            if item_by_year[0] != "CreateTime":
+            if item_by_year[0] != "创建时间":
                 if item_by_year[0].strftime('%Y') == name :
                     ws.append(item_by_year)
 
